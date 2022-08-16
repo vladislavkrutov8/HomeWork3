3b67c3e9baf87e6910de5ac1d21f319217979f87
vladislavkrutov8 <110223646+vladislavkrutov8@users.noreply.github.com>
2022-08-16 14:59:56 +0300

    Обновил папку

---
+++ HomeWork_3/Task_21/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход координаты 
+// двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Write("Введите координаты первой точки x: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты первой точки y: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты первой точки z: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты второй точки x: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты второй точки y: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты второй точки z: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double cord = Math.Sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2));
+
+Console.WriteLine(Math.Round(cord,2));
---
+++ HomeWork_3/Task_23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+int num  = Convert.ToInt32(Console.ReadLine());
+
+void PrintСube (int a)
+{
+    for (int i = 1; i <= a; ++i)
+    Console.WriteLine($"{i} \t {i*i*i}");
+}
+PrintСube (num);
